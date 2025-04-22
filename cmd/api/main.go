2ee857d084f820ac2d12ce894d9fe4e6337e1bf9
nobi <Krittayottoin2541@gmail.com>
2025-04-22 22:14:31 +0700

    Set up Docker Compose for MongoDB, Mongo Express, and app services

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func main() {
+	// โหลดไฟล์ .env
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file")
+	// 	return
+	// }
+
 	fmt.Println("Server is starting...")
 
+	// เชื่อมต่อกับ MongoDB
+	mongoURI := os.Getenv("MONGO_URI")
+	fmt.Println("mongoURI:", mongoURI)
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		fmt.Printf("MongoDB ping failed: %v\n", err)
+		// return หรือ sleep ต่อ แต่ไม่ panic
+		return
+	}
+
+	fmt.Println("Successfully connected to MongoDB!")
+
+	// Routes
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, Docker! <3")
 	})
